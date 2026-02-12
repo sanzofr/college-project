083a9969150d8ad85728e2d12691195eef5fd028
sanzo <sanzofr62@gmail.com>
2026-02-12 23:41:58 +0530

    Moving all zero end

---
+++ zero-toend.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[] = {1, 0, 2, 0, 3, 0, 4, 0, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int j = n - 1;
+    for (int i = 0; i < j; i++)
+    {
+        if (arr[i] == 0)
+        {
+            while (arr[j] == 0 && i < j)
+            {
+                j--;
+            }
+            swap(arr[i], arr[j]);
+            j--;
+        }
+    }
+
+    for (int x = 0; x < n; x++)
+    {
+        cout << arr[x] << "\t";
+    }
+
+    return 0;
+}
