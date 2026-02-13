80ef58ade4101df7576000bcc8ac6b9ac794fb30
sanzo <sanzofr62@gmail.com>
2026-02-13 11:40:36 +0530

    zero to end program

--- zero-toend.cpp
+++ zero-toend.cpp
@@ -6,12 +6,9 @@ int main()
     int arr[] = {1, 0, 2, 0, 3, 0, 4, 0, 5};
     int n = sizeof(arr) / sizeof(arr[0]);
     int j = n - 1;
-    for (int i = 0; i < j; i++)
-    {
-        if (arr[i] == 0)
-        {
-            while (arr[j] == 0 && i < j)
-            {
+    for (int i = 0; i < j; i++) {
+        if (arr[i] == 0) {
+            while (arr[j] == 0 && i < j) {
                 j--;
             }
             swap(arr[i], arr[j]);
@@ -19,8 +16,7 @@ int main()
         }
     }
 
-    for (int x = 0; x < n; x++)
-    {
+    for (int x = 0; x < n; x++) {
         cout << arr[x] << "\t";
     }
 
